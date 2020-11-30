fd5029bdf7903c4f11f1af407c4db3e9b6d9e136
SimplicityCode <70236472+ItsReadyYT@users.noreply.github.com>
2020-11-30 17:11:32 -0500

    actual initial commit

---
+++ floor_adventurer/floor_adventurer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace floor_adventurer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Floor Adventure");
+            Console.WriteLine("By SimpleDeveloping");
+            Console.WriteLine("Press [P] to play, press [X] to quit.");
+            ConsoleKeyInfo
+            cki = Console.ReadKey();
+            do
+            { 
+                cki = Console.ReadKey();
+                if (cki.ToString() == )
+                Console.WriteLine(cki.Key.ToString());
+            } while (cki.Key != ConsoleKey.Escape) ;
+
+        }
+    }
+}
